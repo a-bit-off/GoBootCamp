ba0b28d05f600940cd172559c5296dce32e1e962
Hazzea Sunspot <hazzeasu@ev-c1.kzn.21-school.ru>
2023-05-18 18:48:10 +0300

    finish

--- Go_Day00-0/src/test/go.mod
+++ Go_Day00-0/src/test/go.mod
@@ -2,6 +2,22 @@ module test
 
 replace mean => ../metrics/mean
 
+replace median => ../metrics/median
+
+replace mode => ../metrics/mode
+
+replace sd => ../metrics/sd
+
 go 1.19
 
-require mean v0.0.0-00010101000000-000000000000 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	mean v0.0.0-00010101000000-000000000000 // indirect
+	median v0.0.0-00010101000000-000000000000 // indirect
+	mode v0.0.0-00010101000000-000000000000 // indirect
+	sd v0.0.0-00010101000000-000000000000 // indirect
+)
--- Go_Day00-0/src/test/matrics_test.go
+++ Go_Day00-0/src/test/matrics_test.go
@@ -1,13 +1,162 @@
 package main
 
-import ("testing" "mean")
+import (
+	"github.com/stretchr/testify/assert"
+	"mean"
+	"median"
+	"mode"
+	"sd"
+	"testing"
+)
 
 func TestMean(t *testing.T) {
 	// Arrange
-	
-	var data = []float64{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	expected := 5
+	testTable := []struct {
+		data     []float64
+		expected float64
+	}{
+		{
+			data:     []float64{1, 2, 3, 4, 5, 6, 7, 8, 9},
+			expected: 5.0,
+		},
+		{
+			data:     []float64{3, 2, 5, 6, 3, 4, 5, 9, 6, 5},
+			expected: 4.8,
+		},
+		{
+			data:     []float64{3, 2, 1},
+			expected: 2.0,
+		},
+		{
+			data:     []float64{1, 1},
+			expected: 1,
+		},
+		{
+			data:     []float64{},
+			expected: 0,
+		},
+	}
 
 	// Act
-	num,mean.Mean(data)
+	for _, testCase := range testTable {
+		if result, err := mean.Mean(testCase.data); err == nil {
+			// Assert
+			assert.Equal(t, testCase.expected, result)
+		}
+
+	}
+}
+
+func TestMedian(t *testing.T) {
+	// Arrange
+	testTable := []struct {
+		data     []float64
+		expected float64
+	}{
+		{
+			data:     []float64{1, 2, 3, 4, 5, 6, 7, 8, 9},
+			expected: 5.0,
+		},
+		{
+			data:     []float64{3, 2, 1, 1, 1, 1, 9, 6, 5},
+			expected: 2.0,
+		},
+		{
+			data:     []float64{3, 2, 1, 1, 1, 1, 1},
+			expected: 1.0,
+		},
+		{
+			data:     []float64{1, 1},
+			expected: 1.0,
+		},
+		{
+			data:     []float64{},
+			expected: 0,
+		},
+	}
+
+	// Act
+	for _, testCase := range testTable {
+		if result, err := median.Median(testCase.data); err == nil {
+			// Assert
+			assert.Equal(t, testCase.expected, result)
+		}
+
+	}
+}
+
+func TestMode(t *testing.T) {
+	// Arrange
+	testTable := []struct {
+		data     []float64
+		expected float64
+	}{
+		{
+			data:     []float64{1, 2, 3, 4, 5, 6, 7, 7, 8, 8, 9},
+			expected: 7.0,
+		},
+		{
+			data:     []float64{3, 2, 1, 1, 1, 1, 9, 6, 5},
+			expected: 1.0,
+		},
+		{
+			data:     []float64{3, 2, 1, 1, 1, 1, 1},
+			expected: 1.0,
+		},
+		{
+			data:     []float64{1, 1},
+			expected: 1.0,
+		},
+		{
+			data:     []float64{},
+			expected: 0,
+		},
+	}
+
+	// Act
+	for _, testCase := range testTable {
+		if result, err := mode.Mode(testCase.data); err == nil {
+			// Assert
+			assert.Equal(t, testCase.expected, result)
+		}
+
+	}
+}
+
+func TestSD(t *testing.T) {
+	// Arrange
+	testTable := []struct {
+		data     []float64
+		expected float64
+	}{
+		{
+			data:     []float64{1, 2, 3, 4, 5, 6, 7, 7, 8, 8, 9},
+			expected: 2.5356955783602,
+		},
+		{
+			data:     []float64{3, 2, 1, 1, 1, 1, 9, 6, 5},
+			expected: 2.6988795114425,
+		},
+		{
+			data:     []float64{3, 2, 1, 1, 1, 1, 1},
+			expected: 0.72843135908468,
+		},
+		{
+			data:     []float64{1, 1},
+			expected: 0,
+		},
+		{
+			data:     []float64{},
+			expected: 0,
+		},
+	}
+
+	// Act
+	for _, testCase := range testTable {
+		if result, err := sd.SD(testCase.data); err == nil {
+			// Assert
+			assert.InDelta(t, testCase.expected, result, 0.01)
+		}
+
+	}
 }
