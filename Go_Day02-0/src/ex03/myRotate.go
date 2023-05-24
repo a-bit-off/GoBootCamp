fbccd7179aa31e25a91b229b1d3ddb9202f0e558
Hazzea Sunspot <hazzeasu@va-h5.kzn.21-school.ru>
2023-05-24 14:49:25 +0300

    ex03

--- Go_Day02-0/src/ex03/myRotate.go
+++ Go_Day02-0/src/ex03/myRotate.go
@@ -4,34 +4,44 @@ import (
 	"archive/tar"
 	"compress/gzip"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 )
 
 func main() {
-	files := os.Args[1:]
-
+	fileNames := os.Args[1:]
+	if len(fileNames) == 0 {
+		fmt.Println("Argumens is empty")
+		return
+	}
 	//  поиск архив папки
 	archiveFileName := ""
-	info, err := os.Stat(files[0])
+	info, err := os.Stat(fileNames[0])
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	if info.IsDir() {
-		archiveFileName = files[0]
-		files = files[1:]
+		archiveFileName = fileNames[0]
+		fileNames = fileNames[1:]
 	}
-
-	for _, file := range files {
-		if err := createArchive(file, archiveFileName); err != nil {
-			fmt.Println(err)
-			return
-		}
-
+	var wg sync.WaitGroup
+	for _, name := range fileNames {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			err := createArchive(name, archiveFileName)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}()
 	}
+	wg.Wait()
 }
+
 func createArchive(fileName, archiveFileName string) error {
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -52,59 +62,49 @@ func createArchive(fileName, archiveFileName string) error {
 	}
 	defer archiveFile.Close()
 
-	if err = writeToArchive(fileName, archiveFile); err != nil {
+	if err = writeToArchive(file, archiveFile); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func writeToArchive(fileName string, archiveFile *os.File) error {
+func writeToArchive(file *os.File, archiveFile *os.File) error {
 	gw := gzip.NewWriter(archiveFile)
 	defer gw.Close()
 	tw := tar.NewWriter(gw)
 	defer tw.Close()
 
-	err := addToArchive(tw, fileName)
-	return err
-}
-
-func addToArchive(tw *tar.Writer, filename string) error {
+	if err := addToArchive(tw, file); err != nil {
+		return err
+	}
 	return nil
 }
 
-// func ArchiveFile(fileName string) {
-// 	if err := targz.Compress(fileName, "./"+archiveFileName); err != nil {
-// 		fmt.Println(err)
-// 	}
-// }
+func addToArchive(tw *tar.Writer, file *os.File) error {
+	info, err := file.Stat()
+	if err != nil {
+		return err
+	}
 
-// func main() {
-// 	args := os.Args[1:]
-// 	archiveFileName, err := FindAtchiveFile(args[0])
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	} else {
+	// Создаем заголовок tar из данных FileInfo
+	header, err := tar.FileInfoHeader(info, info.Name())
+	if err != nil {
+		return err
+	}
+	header.Name = file.Name()
 
-// 	}
-// 	fmt.Println(archiveFileName)
-// }
+	// Запись заголовока в tar-архив
+	err = tw.WriteHeader(header)
+	if err != nil {
+		return err
+	}
 
-// func FindAtchiveFile(path string) (string, error) {
-// 	info, err := os.Stat(path)
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	if info.IsDir() {
-// 		return path, nil
-// 	}
-// 	return "", nil
-// }
+	// Копирование содержимое файла в tar-архив
+	_, err = io.Copy(tw, file)
+	if err != nil {
+		return err
+	}
 
-// # Will create file /path/to/logs/some_application_1600785299.tag.gz
-// # where 1600785299 is a UNIX timestamp made from `some_application.log`'s [MTIME](https://linuxize.com/post/linux-touch-command/)
-// ~$ ./myRotate /path/to/logs/some_application.log
-
-// # Will create two tar.gz files with timestamps (one for every log)
-// # and put them into /data/archive directory
-// ~$ ./myRotate -a /data/archive /path/to/logs/some_application.log /path/to/logs/other_application.log
+	return nil
+}
