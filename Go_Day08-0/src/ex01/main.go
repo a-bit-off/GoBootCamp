a822f202625665b51aa10b80d11ffaa087a36ed5
Hazzea Sunspot <hazzeasu@va-h5.kzn.21-school.ru>
2023-06-07 19:30:46 +0300

    ex01 done

---
+++ Go_Day08-0/src/ex01/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type UnknownPlant struct {
+	FlowerType string
+	LeafType   string
+	Color      int `color_scheme:"rgb"`
+}
+
+type AnotherUnknownPlant struct {
+	FlowerColor int
+	LeafType    string
+	Height      int `unit:"inches"`
+}
+
+func main() {
+	describePlant(UnknownPlant{"t1", "l1", 100})
+	fmt.Println()
+	describePlant(AnotherUnknownPlant{200, "l1", 22})
+}
+
+func describePlant(plant any) {
+	t := reflect.TypeOf(plant)
+	v := reflect.ValueOf(plant)
+	fmt.Println(t)
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		tag := ""
+		if string(f.Tag) != "" {
+			tag += "(" + string(f.Tag) + ")"
+		}
+		fmt.Println(f.Name, tag, ":", v.Field(i))
+	}
+}
